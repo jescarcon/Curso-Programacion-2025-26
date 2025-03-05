c50a7c0e8cbd9442bc2af8755b27dd8c98e2c0de
jescarcon <jesuscardenas2000@gmail.com>
2025-03-05 20:18:43 +0100

    Ejercicios Bucles

---
+++ Python/Ejercicios/2.Bucles/2.1For+While.py
@@ -0,0 +1,35 @@
+#Ejercicios sobre bucles Todo con While y For
+
+
+#0.Imprime los elementos de una lista (For/While Basico) ---------------------
+
+
+
+#1.Imprime una lista de los numeros del 1 al 10 (Range) ---------------------
+
+#BONUS. Imprime los numeros del 1 al 100 de 10 en 10 ---------------------
+
+#2.Suma los numeros impares del 5 al 57. (Con If + Acumulador) ---------------------
+
+
+#3.Ordena los nombres en orden alfabetico (Clase anterior) ---------------------
+
+
+#4.Haz una lista con nombres y edades (ZIP) ---------------------
+
+#5 Cual es el niño mas grande de la lista_niños_edad , 2 formas (MAX, Bucle)
+
+#7. Ordena los niños de menor a mayor ---------------------
+
+niños = {
+    "Fran": 12,
+    "Rosa": 19,
+    "Jesus": 24,
+    "Ana": 30,
+    "Carlos": 25,
+}
+
+#8. Haz un top 2 de la lista de antes pista usa [:].
+
+
+#Palabras clave extra sum , max , min
