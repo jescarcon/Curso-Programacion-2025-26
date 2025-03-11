a533a8e4fe632762178cbd353c9779dfcb9f5db1
gonzalogp27 <gonzalogarciaprieto.275@gmail.com>
2025-03-11 17:16:52 +0100

    commit del stash

--- Python/Ejercicios/2.Bucles/2.2 Lista por Comprension.py
+++ Python/Ejercicios/2.Bucles/2.2 Lista por Comprension.py
@@ -1,35 +1,58 @@
 """
 Ejercicios de Listas por Comprensión en Python
 """
+lista=[1,2,3,4,5]
+print(lista[:2])
 
 #region Nivel Fácil
 
 # 1. Generar una lista de los primeros 10 números cuadrados
+ejercicio_facil_1=[x**2 for x in range(1, 11)]
 
 # 2. Crear una lista con los números pares del 1 al 20
+ejercicio_facil_2=[x for x in range(1, 21) if x%2==0]
 
 # 3. Dado un numero quiero una lista donde sus elementos sea su tabla de multiplicar [ '2x1=2', '2x2=4' ...]
+n=4
+ejercicio_facil_3=[f"{n}x{i}={n*i}" for i in range(1,11)]
 
 # 4. Filtrar los números que sean múltiplos de 3 en un rango del 1 al 50
+ejercicio_facil_4=[x for x in range(1, 51) if x%3==0]
 
 # 5. Convertir una lista de palabras en una lista con sus longitudes
 palabras = ["Python", "Listas", "Comprensión"]
+ejercicio_facil_5=[len(palabra) for palabra in palabras]
 
 # 6. Generar una lista con los cuadrados de los números impares del 1 al 20
+ejercicio_facil_6=[x for x in range(1, 21) if x%2==1]
 
 # 7. Obtener una lista con las vocales de una cadena dada
-cadena = "Programación en Python"
+cadena = "PrOgramación en Python"
+ejercicio_facil_7=[caracter for caracter in cadena.lower() if caracter in "aeiouáéíóú"]
+# in 
+# is
 
 #8. Generar una lista de tuplas (número, cuadrado, cubo) para los números del 1 al 10
+n=2
+ejercicio_facil_8=[(i, i**2, i**3) for i in range(1,11)]
 
 # 9. Transformar una matriz (lista de listas) en una lista plana
-matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+matriz = [[1, 2, 3, 4], [4, 5, 6], [7, 8, 9]]
+ejercicio_facil_9=[subElemento for elemento in matriz for subElemento in elemento if len(elemento)>3]
 
 # 10. Dado un diccionario de nombres y edades, obtener una lista con los nombres de las personas mayores de 18 años
 personas = {"Ana": 25, "Luis": 17, "Pedro": 30, "Maria": 15}
 
+#Como lo haría sin lista por comprensión
+# for persona in personas.items():
+#     if persona[1] > 18:
+#         print(persona[0])
+
+ejercicio_facil_10=[persona[0] for persona in personas.items() if persona[1] > 18]
+
 # Imprimir resultados
 #print("Ejercicio 1:", ejercicio_facil_1)
+<<<<<<< Updated upstream
 # print("Ejercicio 2:", ejercicio_facil_2)
 # print("Ejercicio 3:", ejercicio_facil_3)
 # print("Ejercicio 4:", ejercicio_facil_4)
@@ -39,29 +62,53 @@ personas = {"Ana": 25, "Luis": 17, "Pedro": 30, "Maria": 15}
 # print("Ejercicio 8:", ejercicio_facil_8)
 # print("Ejercicio 9:", ejercicio_facil_9)
 # print("Ejercicio 10:", ejercicio_facil_10)
+=======
+#print("Ejercicio 2:", ejercicio_facil_2)
+#print("Ejercicio 3:", ejercicio_facil_3)
+#print("Ejercicio 4:", ejercicio_facil_4)
+#print("Ejercicio 5:", ejercicio_facil_5)
+#print("Ejercicio 6:", ejercicio_facil_6)
+#print("Ejercicio 7:", ejercicio_facil_7)
+#print("Ejercicio 8:", ejercicio_facil_8)
+#print("Ejercicio 9:", ejercicio_facil_9)
+#print("Ejercicio 10:", ejercicio_facil_10)
+>>>>>>> Stashed changes
 
 #endregion
 
 #region Nivel Medio
 
 #1. Mezcla todas las posibilidades entre modelos y pilotos
-
 modelos = ["Boeing 737", "Airbus A320", "Embraer 190"]
 pilotos = ["Piloto 1", "Piloto 2", "Piloto 3"]
 # for modelo in modelos:
 #     for piloto in pilotos:
 #         print(modelo,piloto)
 
+<<<<<<< Updated upstream
 #print([(modelo,piloto) for modelo in modelos for piloto in pilotos])
+=======
+#Sin lista por comprensión
+# for modelo in modelos:
+#     for piloto in pilotos:
+#         modelosPilotos=(modelo, piloto)
+#         print(modelosPilotos)
+ejercicio_medio_1=[(modelo, piloto) for modelo in modelos for piloto in pilotos]
+>>>>>>> Stashed changes
 
 #2.Usando random.randint(x,y) crea combinaciones de (modelos,salida,llegada,pasajeros) BONUS: ¿Y si quiero filtrar por los de +200 pasajeros?
-
 import random
-
 modelos = ["Boeing 737", "Airbus A320", "Embraer 190"]
 ciudades = ["Madrid", "Barcelona", "Lisboa"]
+<<<<<<< Updated upstream
 #print([ (modelo,salida,llegada,random.randint(50,200)) for modelo in modelos for salida in ciudades for llegada in ciudades if salida!=llegada])
+=======
+pasajeros=[random.randint(70,500)]
+salidas=[random.choice(ciudades)]
+llegadas=[random.choice(ciudades)]
+>>>>>>> Stashed changes
 
+ejercicio_medio_2=[(modelo, salida, llegada, pasajero) for modelo in modelos for salida in salidas for llegada in llegadas for pasajero in pasajeros]
 
 # 4. Generar una lista de diccionarios {modelo:pasajeros} con datos 10 ficticios de vuelos. Usa random.choice(modelos) y random.randint(x,y)
 print([{random.choice(modelos):random.randint(50,200)} for _ in range(10)  ])
@@ -91,8 +138,8 @@ vuelos = [
 
 # Imprimir resultados
 
-# print(ejercicio_medio_1)
-# print(ejercicio_medio_2)
+#print(ejercicio_medio_1)
+print(ejercicio_medio_2)
 # print(ejercicio_medio_2_BONUS)
 # print(ejercicio_medio_3)
 # print(ejercicio_medio_4)
