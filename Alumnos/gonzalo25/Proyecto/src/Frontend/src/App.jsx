956d659fbde7cc3f816864e99f5a0ee3698cf7f1
gonzalogp27 <gonzalogarciaprieto.275@gmail.com>
2025-03-25 16:18:34 +0100

    Revert "clase de hoy"
    This reverts commit 81c6f0035fd7946b07e2393fbc3b8b12effd1026.

--- Alumnos/gonzalo25/Proyecto/src/Frontend/src/App.jsx
+++ Alumnos/gonzalo25/Proyecto/src/Frontend/src/App.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
-import Note from './components/note/note.jsx'
+import { useState,useEffect } from 'react';
+        //cargar    usar
+import './App.css';
+import {BASE_API_URL} from './constants';
 
 function App() {
+  //#region Variables
+    const [notes,setNotes]= useState([]);
+  //#endregion
+
+  //#region Logica
+  useEffect(()=>{ //siempre se va a ejecutar
+    const fetchNotes = async ()=>{
+      try {
+        const response = await fetch(`${BASE_API_URL}/api/noteApp/notes`);
+        if(!response.ok){
+          throw new Error(`Error: ${response.status} ${response.statusText}`);
+        }
+        const data = await response.json();
+        setNotes(data);
+
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    }
+    fetchNotes();
+  },[]);
+
+  //#endregion
+
+
   return (
-    <Note/>
+    <div>
+      <h1>Aplicación notas by Gonzalo</h1>
+      <ul>
+        {notes.length > 0 ? (
+          notes.map((note) => {
+            return (
+              <li key={note.id}>
+                <p>Titulo: {note.name}</p>
+                <p>Descripcion: {note.description}</p>
+              </li>
+            )
+          })
+        ) : (<p>No hay notas disponibles.</p>)
+        }
+      </ul>
+    </div>
   )
 }
 
