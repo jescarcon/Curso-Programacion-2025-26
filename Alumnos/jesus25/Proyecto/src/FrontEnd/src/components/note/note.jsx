c10506afb9963c6b0a12c1c196e307ba590ad3a6
jescarcon <jesuscardenas2000@gmail.com>
2025-04-01 14:59:47 +0200

    Note completo

--- Alumnos/jesus25/Proyecto/src/FrontEnd/src/components/note/note.jsx
+++ Alumnos/jesus25/Proyecto/src/FrontEnd/src/components/note/note.jsx
@@ -1,7 +1,9 @@
-
 import { useState, useEffect } from 'react';
 import './note.css';
 import { BASE_API_URL } from './../../constants';
+import addIcon from '/assets/images/common/black-add-icon.png';
+
+import Modal from '../modal/modal';
 
 function Note() {
   //#region Variables
@@ -16,11 +18,64 @@ function Note() {
   const [editName, setEditName] = useState("");
   const [editDescription, setEditDescription] = useState("");
 
+  //Variables Modal
+  const [isCreateOpen, setIsCreateOpen] = useState(false);
+  const [isEditOpen, setIsEditOpen] = useState(false);
+
+  //Variable Menú Contextual
+  const [contextMenu, setContextMenu] = useState(null);  
 
   //#endregion
 
   //#region Logic
 
+  //#region Menú Contextual
+  const handleContextMenu = (e, note) => {
+    e.preventDefault();
+    setContextMenu({ x: e.clientX, y: e.clientY, note });
+  };
+
+  const handleContextAction = (action) => {
+    if (action === 'edit') {
+      setEditName(contextMenu.note.name);
+      setEditDescription(contextMenu.note.description);
+      setEditNote(contextMenu.note);
+      setIsEditOpen(true);
+    } else if (action === 'delete') {
+      handleDelete(contextMenu.note.id);
+    }
+    setContextMenu(null); // Cerrar el menú
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      if (contextMenu && !e.target.closest('.context-menu')) {
+        setContextMenu(null); // Cerrar el menú
+      }
+    };
+  
+    // Escuchar clics en todo el documento
+    document.addEventListener('click', handleClickOutside);
+  
+    // Limpiar el event listener cuando el componente se desmonte
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [contextMenu]);
+  
+  //#endregion
+
+  // Cuando se cierra el modal, limpiar los campos
+  const handleCloseCreateModal = () => {
+    setIsCreateOpen(false);
+    setName(""); // Limpiar el campo del nombre
+    setDescription(""); // Limpiar el campo de descripción
+  };
+
+
+  //#region CRUD
+
+  //Funcion List
   useEffect(() => {
     const fetchNotes = async () => {
       try {
@@ -42,7 +97,6 @@ function Note() {
   const createNote = async (e) => {
     e.preventDefault();
 
-
     const formData = new FormData();
     formData.append("name", name);
     formData.append("description", description);
@@ -62,6 +116,7 @@ function Note() {
 
       setName("");
       setDescription("");
+      setIsCreateOpen(false);
     } catch (error) {
       console.error("Error en la creacion de la nota:", error);
     }
@@ -92,11 +147,13 @@ function Note() {
       );
 
       setEditNote(null);
+      setIsEditOpen(false);
+
     } catch (error) {
       console.error("Error en la edición de la nota: ", error);
     }
   };
-  
+
   //Funcion Delete
   const handleDelete = async (id) => {
     try {
@@ -113,93 +170,66 @@ function Note() {
       console.error("Error al eliminar la nota:", error);
     }
   };
-
+  //#endregion
 
   //#endregion
 
   return (
     <div className='note-container'>
-      <div className='note-header'>Notas de Jesús</div>
+      <div className='note-header'>
+        <div className='note-header-text'>Notas de Jesús</div>
+        <div className='add-note-button' onClick={() => setIsCreateOpen(true)}>
+          <img src={addIcon} alt="Add Note" />
+        </div>
+      </div>
+
       <div className='note-elements'>
         {notes.length > 0 ? (
           notes.map((note) => (
-            <div key={note.id} className='note-element'>
+            <div key={note.id} className='note-element' onContextMenu={(e) => handleContextMenu(e, note)}>
               <p className='note-element-name'>{note.name}</p>
               <p className='note-element-description'>{note.description}</p>
-              <button onClick={() => {
-                setEditNote(note);
-                setEditName(note.name);
-                setEditDescription(note.description);
-              }}>
-                Editar
-              </button>
-
-              <button onClick={() => handleDelete(note.id)}>Eliminar</button>
             </div>
           ))
         ) : (
           <p>No hay notas disponibles.</p>
         )}
       </div>
-
-
-
-
-      {/* Formulario Crear Notas */}
-      <div className='create-note-form-container'>
-        <div className='create-note-form-header'><h2>Crear Nota</h2></div>
-        <form onSubmit={createNote}>
-          <div className='create-note-form-group'>
-            <label>Título:</label>
-            <input type="text" id="name" name="name"
-              value={name} onChange={(e) => setName(e.target.value)}
-              required />
-          </div>
-
-          <div className='create-note-form-group'>
-            <label>Descripción:</label>
-            <input type="text" id="description" name="description"
-              value={description} onChange={(e) => setDescription(e.target.value)}
-              required />
-          </div>
-          <button type='submit'>Crear Nota</button>
-        </form>
-      </div>
       
-      {/* Formulario Editar Notas */}
-      {editNote && (
-        <div className='edit-note-form-container'>
-          <div className='edit-note-form-header'><h2>Editar Nota</h2></div>
-          <form onSubmit={handleEdit}>
-            <div className='edit-note-form-group'>
-              <label>Título:</label>
-              <input
-                type="text"
-                id="editName"
-                name="editName"
-                value={editName}
-                onChange={(e) => setEditName(e.target.value)}
-                required
-              />
-            </div>
-
-            <div className='edit-note-form-group'>
-              <label>Descripción:</label>
-              <input
-                type="text"
-                id="editDescription"
-                name="editDescription"
-                value={editDescription}
-                onChange={(e) => setEditDescription(e.target.value)}
-                required
-              />
-            </div>
-
-            <button type='submit'>Guardar Cambios</button>
-            <button type="button" onClick={() => setEditNote(null)}>Cancelar</button>
-          </form>
+      {contextMenu && (
+        <div className="context-menu" style={{ top: contextMenu.y, left: contextMenu.x }}>
+          <button onClick={() => handleContextAction('edit')}>Editar</button>
+          <button onClick={() => handleContextAction('delete')}>Eliminar</button>
         </div>
       )}
+
+      <Modal isOpen={isCreateOpen} onClose={() => handleCloseCreateModal(false)}>
+        <h2>Crear Nota</h2>
+        <form onSubmit={createNote}>
+          <label>Título:</label>
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
+
+          <label>Descripción:</label>
+          <input type="text" value={description} onChange={(e) => setDescription(e.target.value)} required />
+
+          <button type="submit">Crear</button>
+          <button onClick={() => handleCloseCreateModal(false)}>Cancelar</button>
+        </form>
+      </Modal>
+
+      <Modal isOpen={isEditOpen} onClose={() => setIsEditOpen(false)}>
+        <h2>Editar Nota</h2>
+        <form onSubmit={handleEdit}>
+          <label>Título:</label>
+          <input type="text" value={editName} onChange={(e) => setEditName(e.target.value)} required />
+
+          <label>Descripción:</label>
+          <input type="text" value={editDescription} onChange={(e) => setEditDescription(e.target.value)} required />
+
+          <button type="submit">Guardar Cambios</button>
+          <button onClick={() => setIsEditOpen(false)}>Cancelar</button>
+        </form>
+      </Modal>
 
     </div>
   );
